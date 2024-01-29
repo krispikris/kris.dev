acfe2320d6778c66c79c2bd26266dc39287d2d53
krispikris <kris@kristopherhan.com>
2024-01-28 22:50:57 -0800

    label

--- src/pages/404/404.js
+++ src/pages/404/404.js
@@ -74,7 +74,7 @@ export function Page404() {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Enter Text
+                Enter Text Here
               </a>
             </div>
           </Fragment>
