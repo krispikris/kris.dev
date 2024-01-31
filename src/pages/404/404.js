1c4cfab73ed7395f6319b84d9b72845fed000ab6
krispikris <kris@kristopherhan.com>
2024-01-30 23:50:39 -0800

    href

--- src/pages/404/404.js
+++ src/pages/404/404.js
@@ -74,7 +74,7 @@ export function Page404() {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Testing Text. Word.
+                Enter text here.
               </a>
             </div>
           </Fragment>
