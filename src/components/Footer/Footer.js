94e6e93cd763b2f0b013e75e94b61b5a7e4ddfed
krispikris <kris@kristopherhan.com>
2024-02-20 22:06:39 -0800

    span

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,9 @@ import styles from './Footer.module.css';
 export const Footer = ({ className }) => (
   <footer className={classes(styles.footer, className)}>
     <Text size="s" align="center">
-      <span className={styles.date}>{`© ${new Date().getFullYear()} Kris Han`}</span>
+      <span
+        className={styles.date}
+      >{`© ${new Date().getFullYear()} Kristopher Han`}</span>
       {/* <Link secondary className={styles.link} href="/humans.txt" target="_self">
         Crafted by yours truly
       </Link> */}
